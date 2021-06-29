124cd26efcff5fc8efdaa3e95c41c0ce2f150e79
Arash Zaheri <arashzaheri@gmail.com>
2021-06-29 17:09:43 -0500

    final

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { allowedNodeEnvironmentFlags } = require('process');
 
 var Rollbar = require("rollbar");
 var rollbar = new Rollbar({
@@ -11,12 +12,39 @@ var rollbar = new Rollbar({
 
 app.use(express.json());
 
+let vacationSpots = [] //added spots here
+
 app.get('/', function(req, res) {
     rollbar.log('Hello There')
 
     rollbar.error('User tried to acess incorrect path')
 
     res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.post('/api/vacationspots', function(req, res) {
+  let {name} = req.body
+  name = name.trim()
+  const index = vacationSpots.findIndex(function(vName) { //finds if spot exists
+    return vName === name
+  })
+  console.log(index)
+
+  try {
+    if (index === -1 && name !== '') {
+      vacationSpots.push(name)
+      rollbar.log('Vacation spot added successfully', {author:'riley', type:'manual'})
+      res.status(200).send(vacationSpots)
+    } else if (name === '') {
+      rollbar.error('no name given')
+      res.status(400).send('must provide location')
+    } else {
+      rollbar.error('location exists')
+      res.status(400).send('location already exists')
+    }
+  } catch(err) {
+    rollbar.error(err)
+  }
 })
 
 const port = process.env.PORT || 5000

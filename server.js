8a719e9089770b0d84f74f416543f5f94f438d8a
Arash Zaheri <arashzaheri@gmail.com>
2021-06-29 15:03:59 -0500

    inital

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '631c284bd8ab471d83a28722061657a3',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
 app.use(express.json());
 
+app.get('/', function(req, res) {
+    rollbar.log('Hello There')
 
+    rollbar.error('User tried to acess incorrect path')
+
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
 
 const port = process.env.port || 4000
 app.listen(port, function () {
